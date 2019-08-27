2e9428f3ca9137d28d6f348e5441ef57561b0deb
Hermann Barbe <hermann.barbe@med.uni-goettingen.de>
2019-08-27 14:31:27 +0200

    Update: soundfiles for laura_li + script for creating new soundfiles

---
+++ soundfiles/Laura/tts-script-laura.py
@@ -0,0 +1,44 @@
+# -*- coding: iso-8859-1 -*-
+
+"""Synthesizes speech from the input string of text or ssml.
+
+Note: ssml must be well-formed according to:
+    https://www.w3.org/TR/speech-synthesis/
+"""
+from google.cloud import texttospeech
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.types.VoiceSelectionParams(
+    language_code='de-DE',
+    name='de-DE-Wavenet-C',
+    ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.types.AudioConfig(
+    pitch=6.4,
+    speaking_rate=0.96,
+    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+with open("outputlaura.txt") as f:
+    for line in f:
+        # Set the text input to be synthesized
+        transcribe = line
+        synthesis_input = texttospeech.types.SynthesisInput(text=transcribe)  
+        filename = line.replace(" ","") 
+        filename = filename.replace(".","") 
+        filename = filename.replace(",","") 
+        filename = filename.replace("?","") 
+        filename = filename.replace("\n","") 
+        filename = filename.replace("!","") + ".mp3"
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        response = client.synthesize_speech(synthesis_input, voice, audio_config)   
+        # The response's audio_content is binary.
+        with open(filename, 'wb') as out:
+             # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file',filename)
