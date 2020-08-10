a05680c2f3399ae6453900d286bc0d4e3c1a95b2
Hermann Barbe <hermannbarbe@googlemail.com>
2020-08-10 08:49:04 +0200

    bugfix for bad answers although randnum is good

--- soundfiles/Jason/tts_oneline_jason.py
+++ soundfiles/Jason/tts_oneline_jason.py
@@ -13,7 +13,7 @@ from google.cloud import texttospeech
 client = texttospeech.TextToSpeechClient()
 
 # Set the text input to be synthesized
-transcribe = "wir sind team blau bei pokemon. kevin hat die besten pokemon!"
+transcribe = "tschüs"
 synthesis_input = texttospeech.types.SynthesisInput(text=transcribe)
 
 # Build the voice request, select the language code ("en-US") and the ssml
