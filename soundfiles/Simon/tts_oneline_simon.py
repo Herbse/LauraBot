4b9e42635c7deac998f98421c68281592cae9ed4
Hermann Barbe <hermannbarbe@googlemail.com>
2020-10-19 09:01:51 +0200

    Soundfile Update

--- soundfiles/Simon/tts_oneline_simon.py
+++ soundfiles/Simon/tts_oneline_simon.py
@@ -13,7 +13,7 @@ from google.cloud import texttospeech
 client = texttospeech.TextToSpeechClient()
 
 # Set the text input to be synthesized
-transcribe = "tschüs"
+transcribe = "ich würde viel lieber wieder zu herrn schaller gehen. Er ist der beste lehrer."
 synthesis_input = texttospeech.types.SynthesisInput(text=transcribe)
 
 # Build the voice request, select the language code ("en-US") and the ssml
